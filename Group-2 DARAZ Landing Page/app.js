71a45e981fd772ad3dd08cab1cf3763fce16d67e
Aziz Ur Rehman <111462831+rehman2346900@users.noreply.github.com>
2022-12-03 23:20:37 -0800

    Add files via upload

---
+++ Group-2 DARAZ Landing Page/app.js
@@ -0,0 +1,9 @@
+// const head = document.querySelector(".head");
+
+// function scrollFunc() {
+//   let scrollValue = window.scrollY;
+
+//   if(scrollValue > 100){
+//     head.classList.add("hidden");
+//   }
+// }
